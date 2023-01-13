dce81fefc971716f9795b7fec5213e821c6209e0
dylan <dylanc@lavishflo.com>
2023-01-12 19:56:50 -0500

    f

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
+scheduler.addSimpleIntervalJob(job);
 const dotenv = require("dotenv");
 dotenv.config();
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const { MessagingResponse } = require("twilio").twiml;
-const client = require("twilio")(accountSid, authToken);
-
-client.messages
-  .create({
-    body: "This is the ship thaun in fourteen parsecs?",
-    from: "+19143353478",
-    to: "+19143866407",
-  })
-  .then((message) => console.log(message.sid));
+const { sendSms } = require("./twilio");
