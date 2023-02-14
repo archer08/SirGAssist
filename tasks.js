7df0878acae19caa3576a6c65e07c5495ac5498e
dylan <dylanc@lavishflo.com>
2023-02-13 20:24:30 -0500

    k

--- tasks.js
+++ tasks.js
@@ -5,13 +5,13 @@ const scheduler = new ToadScheduler();
 
 const SendSms = new Task("check for messages and send them out", async () => {
   const messages = await Message.find({ sent: false }, (err, messages) => {
-    console.log(messages);
     // sendSms(
     //   "Hello World",
     //   process.env.MY_PHONE_NUMBER,
     //   process.env.TWILIO_PHONE_NUMBER
     // );
   });
+  console.log(messages);
 });
 const job = new SimpleIntervalJob({ seconds: 5 }, SendSms);
 module.exports = { job };
